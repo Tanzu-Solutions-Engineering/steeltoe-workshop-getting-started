c1eca299239ebdec54f445ad6387c081250e63de
Andrew Stakhov <andrew.stakhov@gmail.com>
2021-08-23 17:01:29 -0400

    Change url

--- services/_run-config-server.bat
+++ services/_run-config-server.bat
@@ -1,2 +1,2 @@
-set SPRING_CLOUD_CONFIG_SERVER_GIT_URI=https://github.com/macsux/workshop-config-repo.git
+set SPRING_CLOUD_CONFIG_SERVER_GIT_URI=https://github.com/Tanzu-Solutions-Engineering/steeltoe-workshop-config-repo.git
 java -jar config-server-2.5.3.jar
